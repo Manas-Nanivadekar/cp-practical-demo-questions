07227f6b468ffa20d584409bb518425b75367b37
Manas-Nanivadekar <manasnanivadekar@gmail.com>
2023-06-06 18:22:31 +0530

    q5 add

---
+++ q5.c
@@ -0,0 +1,21 @@
+// Program to print the following pattern
+// A
+// AB
+// ABC
+// ABCD
+// ABCDE
+
+#include <stdio.h>
+
+void main()
+{
+    int i, j;
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("%c", 64 + j);
+        }
+        printf("\n");
+    }
+}
